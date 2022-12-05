904573f4b7453d698ee45585dddc9c62c2637ad1
Samandareshmamatov <samandareshmamatov1127@gmail.com>
2022-12-05 21:57:09 +0500

    Add this project

---
+++ main.js
@@ -0,0 +1,15 @@
+const btn1 = document.querySelector("#btn1")
+const btn2 = document.querySelector("#btn2")
+const text = document.querySelector("#text")
+let number = 0;
+
+function addNumber() {
+    text.innerText = ++number;
+}
+
+function deleteNumber() {
+    text.innerText = --number;
+}
+
+btn1.addEventListener("click", addNumber)
+btn2.addEventListener("click", deleteNumber)
